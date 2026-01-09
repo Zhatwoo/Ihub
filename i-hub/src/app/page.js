fabb0f87d7d3a8b01020334034c4abedcf556cec
Neo Dela Torre <ndelatorre08252002@gmail.com>
2026-01-09 15:55:34 +0800

    finish landing page picture nalang

--- i-hub/src/app/page.js
+++ i-hub/src/app/page.js
@@ -2,8 +2,11 @@ import Header from './landingpage/components/header';
 import Hero from './landingpage/hero';
 import TrustedPartners from './landingpage/components/trustedPartners';
 import FlexibleWorkspace from './landingpage/components/flexibleworkspace';
+import AboutInspire from './landingpage/components/aboutInspire';
 import './landingpage/globals.css';
-
+import AvailableRentals from './landingpage/components/availableRentals';
+import LocationCta from './landingpage/components/locationCta';
+import Footer from './landingpage/components/footer';
 export default function Home() {
   return (
     <div className="min-h-screen">
@@ -11,6 +14,10 @@ export default function Home() {
       <Hero />
       <TrustedPartners />
       <FlexibleWorkspace />
+      <AvailableRentals />
+      <AboutInspire />
+      <LocationCta />
+      <Footer />
     </div>
   );
 }
