4aee385fd9b1cf183df9c01881d6870e0ad660c8
Neo Dela Torre <ndelatorre08252002@gmail.com>
2026-01-09 10:37:24 +0800

    globs ok

--- i-hub/src/app/page.js
+++ i-hub/src/app/page.js
@@ -2,6 +2,8 @@ import Header from './landingpage/components/header';
 import Hero from './landingpage/hero';
 import TrustedPartners from './landingpage/components/trustedPartners';
 import FlexibleWorkspace from './landingpage/components/flexibleworkspace';
+import './landingpage/globals.css';
+
 export default function Home() {
   return (
     <div className="min-h-screen">
